7d20bd5446d1f91732bf013c320c42e8e2053f0f
Mary <l.skyling@gmail.com>
2018-08-26 12:25:25 -0400

    set devise and add name and role colums

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_24_152547) do
+ActiveRecord::Schema.define(version: 2018_08_24_203701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,20 +28,6 @@ ActiveRecord::Schema.define(version: 2018_08_24_152547) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.integer "class_hours"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "instructors", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "nickname"
-    t.integer "age"
-    t.string "abilities"
-    t.integer "salary"
-    t.string "photo_url"
-    t.string "education"
-    t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -69,12 +55,15 @@ ActiveRecord::Schema.define(version: 2018_08_24_152547) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'bcrypt'
 
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
