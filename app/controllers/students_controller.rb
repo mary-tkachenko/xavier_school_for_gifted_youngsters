8ec3f2be1501195935ec080d1da1ab8514101525
Mary <l.skyling@gmail.com>
2018-08-26 21:32:54 -0400

    limited access for users who didnt signin and also limited posibilitiesfor some roles

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,7 @@
 class StudentsController < ApplicationController
+
+    before_action :allow_only_admin, only: [:new, :create, :edit]
+
     def index
         # method: get
         # action: index
@@ -13,6 +16,7 @@ class StudentsController < ApplicationController
       end
     
       def new
+
         # method: get
         # action: index
         # template: students/new.html.erb
@@ -21,7 +25,6 @@ class StudentsController < ApplicationController
 
       def create
         Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name], nickname: params[:student][:nickname], age: params[:student][:age], abilities: params[:student][:abilities], photo: params[:student][:photo], cohort_id: params[:student][:cohort_id])
-
         redirect_to "/students"
       end
     
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_24_203701) do
+ActiveRecord::Schema.define(version: 2018_08_26_221612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,20 @@ ActiveRecord::Schema.define(version: 2018_08_24_203701) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.integer "class_hours"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "instructors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "nickname"
+    t.integer "age"
+    t.string "abilities"
+    t.integer "salary"
+    t.string "photo_url"
+    t.string "education"
+    t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
