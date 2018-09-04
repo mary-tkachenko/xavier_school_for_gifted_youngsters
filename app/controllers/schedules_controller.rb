36dec97e6995d6c2872c388c3bdd81df9ae22835
Mary <l.skyling@gmail.com>
2018-09-04 10:52:18 -0400

    functionality for schedules. styling all index pages

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -9,6 +9,15 @@ class SchedulesController < ApplicationController
       end
     
       def show
+        @cohort = Cohort.find(params[:id])
+
+        @schedule_days = {};
+        Schedule.days_of_week.each do |day|
+          results = Schedule.where({ cohort_id: @cohort.id, day_of_week: day }).order(:time)
+          if results.count > 0
+            @schedule_days[day] = results
+          end
+        end
       end
     
       def new
@@ -19,7 +28,13 @@ class SchedulesController < ApplicationController
       end
 
       def create
-        Schedule.create(day_of_week: params[:schedule][:day_of_week], time: params[:schedule][:time], course_id: params[:schedule][:course_id], cohort_id: params[:schedule][:cohort_id], instructor_id: params[:schedule][:instructor_id])
+        Schedule.create!(
+          day_of_week: params[:schedule][:day_of_week],
+          time: params[:schedule][:time],
+          course_id: params[:schedule][:course_id],
+          cohort_id: params[:schedule][:cohort_id],
+          instructor_id: params[:schedule][:instructor_id]
+        )
         redirect_to "/schedules"
       end
     
@@ -47,6 +62,10 @@ class SchedulesController < ApplicationController
       def destroy
         @schedule = Schedule.find(params[:id])
         @schedule.destroy
-        redirect_to "/schedules"
+        # Do not redirect when requested with AJAX
+        respond_to do |format|
+          format.js {  }
+          format.all { redirect_to "/schedules" }
+        end
       end
 end
